5f3568f000e929ef675b131c6dfc10cf0cfed6d1
Muksam <Muksam@example.com>
2022-06-18 15:08:35 +0545

    'removing-jwt-token'

--- api/views.py
+++ api/views.py
@@ -47,6 +47,7 @@ class LoginAPI(KnoxLoginView):
 		serializer = AuthTokenSerializer(data=request.data)
 		serializer.is_valid(raise_exception=True)
 		user = serializer.validated_data['user']
+
 		login(request, user)
 		return super(LoginAPI, self).post(request, format=None)
 
